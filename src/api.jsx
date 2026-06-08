0d85b652790bc107f8c826e14d0201ec239dbc08
Vishva <vishva2431email@gmail.com>
2026-06-08 11:11:54 +0530

    changes in api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const API = axios.create({
   baseURL: API_BASE_URL,
